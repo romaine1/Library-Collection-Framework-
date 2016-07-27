5c5869ff6afda2bad3d4c5d57744efad530bb53b
romaine1 <romaine1@naver.com>
2016-07-27 15:44:42 +0900

    라이브러리 컬렉션 연습

---
+++ 라이브러리(컬렉션)/src/com/sist/sawon/MainClass.java
@@ -0,0 +1,41 @@
+package com.sist.sawon;
+
+import java.util.ArrayList;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ArrayList<SawonVO> list = new ArrayList<>();
+		SawonVO vo = new SawonVO();
+		
+		vo.setSabun(1);
+		vo.setName("정도전");
+		vo.setJob("대리");
+		vo.setDname("영엉부");
+		vo.setLoc("서울");
+		vo.setPay(3200);
+		list.add(vo);
+		
+		vo = new SawonVO();
+
+		vo.setSabun(2);
+		vo.setName("이성계");
+		vo.setJob("사원");
+		vo.setDname("기획부");
+		vo.setLoc("경기");
+		vo.setPay(2600);
+		list.add(vo);
+		
+		vo = new SawonVO();
+		vo.setSabun(3);
+		vo.setName("정몽주");
+		vo.setJob("과장");
+		vo.setDname("개발부");
+		vo.setLoc("서울");
+		vo.setPay(4000);
+		list.add(vo);
+
+		for(SawonVO v : list){
+			System.out.println(v.getSabun()+"\t"+v.getName()+"\t"+v.getDname()+"\t"+v.getLoc()+"\t"+v.getJob()+"\t"+v.getPay());
+		}
+	}
+}
